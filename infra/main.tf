533e2e633c5dbea4fdc968ecd55a451121656733
Vinicius Garcia Imendes Dechechi <151979143+vdechechi@users.noreply.github.com>
2026-06-23 18:22:26 -0300

    feat: transforma o fraud-check em lambda

--- infra/main.tf
+++ infra/main.tf
@@ -20,6 +20,7 @@ provider "aws" {
     dynamodb = "http://localhost:4566"
     sns      = "http://localhost:4566"
     lambda   = "http://localhost:4566"
+    iam      = "http://localhost:4566"
   }
 }
 
@@ -45,4 +46,45 @@ resource "aws_dynamodb_table" "payment_events" {
     name = "createdAt"
     type = "S"
   }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "fraud-check-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_lambda_function" "fraud_check" {
+  filename         = "fraud-check.zip"
+  function_name    = "fraud-check"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "python3.12"
+  source_code_hash = filebase64sha256("fraud-check.zip")
+
+  environment {
+    variables = {
+      AWS_ENDPOINT   = "http://localhost:4566"
+      QUEUE_URL      = "http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/fraud-check-queue"
+      FRAUD_THRESHOLD = "5000"
+    }
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_trigger" {
+  event_source_arn = aws_sqs_queue.fraud_check_queue.arn
+  function_name    = aws_lambda_function.fraud_check.arn
+  batch_size       = 10
+  enabled          = true
 }
